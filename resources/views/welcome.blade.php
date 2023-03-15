e70dedd2d6e6e8fabe5043d002741fdc749e1e22
Sonu Paudel <86399801+SonuPaudel@users.noreply.github.com>
2023-03-15 09:47:51 +0545

    after breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('master')
 @section('content')
-<h2 class="bg-red-800 text-white p-4 font-bold"> Hello this Home page</h2>
+<h2 class="bg-red-800 text-white p-4 font-bold"> Hello this Dashboard page</h2>
 <h1 class="font-bold">My name is {{$name}}</h1>
 @endsection
