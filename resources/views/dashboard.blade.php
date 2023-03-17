af141f87c6c7edba10035185bb67c94f3a70158e
Sonu Paudel <86399801+SonuPaudel@users.noreply.github.com>
2023-03-17 09:38:02 +0545

    added dashboard content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.app')
+@section('content')
+<h2 class="text-4xl font-bold border-b-4 text-blue-300 border-black">Dashboard</h2>
+<div class="grid grid-cols-3 gap-8 my-6">
+    <div class="p-4 flex justify-between bg-green-600 text-white rounded-lg">
+        <span class="text-xl font-bold">Total News</span>
+        <span class="texxt-5xl font-bold">665</span>
+    </div>
+    <div class="p-4 flex justify-between bg-blue-600 text-white rounded-lg">
+        <span class="text-xl font-bold">Categories</span>
+        <span class="texxt-5xl font-bold">665</span>
+    </div>
+    <div class="p-4 flex justify-between bg-red-600 text-white rounded-lg">
+        <span class="text-xl font-bold">Total Visits</span>
+        <span class="texxt-5xl font-bold">665</span>
+    </div>
+</div>
+@endsection
